c4479d2008962309f79147d23dc28a8f75622970
Badar Maulana <45702477+badadarr@users.noreply.github.com>
2020-12-18 20:21:05 +0700

    boo

--- CourseApp/ContentView.swift
+++ CourseApp/ContentView.swift
@@ -9,8 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Home()
     }
 }
 
@@ -19,3 +18,55 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+struct Home : View {
+    var body: some View {
+        
+        VStack {
+            
+            HStack {
+                
+                VStack(alignment: .leading, spacing: 10) {
+                    Text("Hello Badadarr")
+                        .font(.title)
+                        .fontWeight(.bold)
+                    
+                    Text("Ayok Upgrade Skill Kamu!!")
+                }
+                .foregroundColor(.black)
+                
+                Spacer(minLength: 0)
+                
+                Button(action: {}) {
+                    Image("profile")
+                        .resizable()
+                        .renderingMode(.template)
+                        .frame(width: 50, height: 50)
+                }
+            }
+            .padding()
+            
+            Spacer(minLength: 0)
+        }
+        .background(Color.black.opacity(0.05).ignoresSafeArea(.all, edges: .all))
+    }
+}
+
+// Model Data...
+struct Course : Identifiable {
+    
+    var id = UUID().uuidString
+    var name : String
+    var numCourse : Int
+    var asset : String
+}
+
+var courses = [
+    
+    Course(name: "Coding", numCourse: 12, asset: "coding"),
+    Course(name: "Trading", numCourse: 12, asset: "trading"),
+    Course(name: "Cooking", numCourse: 12, asset: "cooking"),
+    Course(name: "Marketing", numCourse: 12, asset: "marketing"),
+    Course(name: "UI/UX", numCourse: 12, asset: "uiux"),
+    Course(name: "Digital Marketing", numCourse: 12, asset: "digital")
+]
