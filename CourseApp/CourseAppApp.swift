199b67f55467c0dc70d78ebdf1fddb9c7301ca7f
Badar Maulana <45702477+badadarr@users.noreply.github.com>
2020-12-18 20:19:10 +0700

    Boo

---
+++ CourseApp/CourseAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CourseAppApp.swift
+//  CourseApp
+//
+//  Created by badar on 18/12/20.
+//
+
+import SwiftUI
+
+@main
+struct CourseAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
